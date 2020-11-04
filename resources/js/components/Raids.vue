b8143dce87e29251e70c6c3cbc0d0ab08016c3d8
Nikolaos Christos Antoniadis <nikosantoniadis94@gmail.com>
2020-11-04 13:09:07 +0200

    Branch Test

--- resources/js/components/Raids.vue
+++ resources/js/components/Raids.vue
@@ -70,7 +70,7 @@
                         <div class="card">
                             <div class="card-image">
                                 <figure class="image is-2by2">
-                                    <img alt="Placeholder image" :src="'images/icon_' + pokemon[raid.pokemon_id - 1].name.toLowerCase() + '.jpg'" />
+                                    <img alt="Placeholder image" :src="'images/icon_' + pokemon[raid.pokemon_id - 1].name.toLowerCase() + '.png'" />
                                 </figure>
                             </div>
                             <div class="card-content">
